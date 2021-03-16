d83a75d5601128ea03d566aa6bef9a44e02aef1a
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-03-16 19:55:59 -0400

    not honestly sure what i accomplished since last push, but pushing because it needs to be done

--- assets/script.js
+++ assets/script.js
@@ -18,25 +18,20 @@ function buildApiUrl() {
     var city = document.querySelector('#inlineFormInputCity').value.trim();
     var apiUrl = apiUrlA.concat(city, apiUrlB);
     console.log(apiUrl)
+
+
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                console.log(data[i].url);
+            }
+        });
+
 }
-
-
-
-var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=pittsburgh&appid=5e396ffdb012177df336e70811fd23a0';
-var responseText = document.getElementById('response-text');
-
-fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        // console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            console.log(data[i].url);
-        }
-    });
-
-
 
 
 fetchButton.addEventListener('click', knowCity)
