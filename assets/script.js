fb75db0ead574f31f619523b86cde7e9e52014d8
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-03-16 22:46:04 -0400

    end of day save point

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,45 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+
+
+
+
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+
+
 
 var getCity = document.getElementById("citySearch")
 var showWeather = document.getElementById("weatherBox");
 var fetchButton = document.getElementById("showTheWeather");
+var cityListEl = $('#cityList');
 
 
 function knowCity() {
     event.preventDefault();
-    var city = document.querySelector('#inlineFormInputCity').value.trim();
+    var city = document.querySelector('#inlineFormInputCity').value.replace(/\s/g, "");
+    var cityListEl = $('#cityList').value;
     console.log(city)
 
-    buildApiUrl()
+    cityListEl.append('<li>' + city + '</li>');
+
+
+      buildApiUrl()
 }
+
+// appendCityList
+// // create Element
+// // add content 
+// // append to document 
+
 
 function buildApiUrl() {
     var apiUrlA = 'https://api.openweathermap.org/data/2.5/weather?q='
